bcd1705cc407f6c3f4914dda2303dca5c784fa32
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-01-20 15:51:51 -0800

    Create puzzle-34.js

---
+++ puzzle-34.js
@@ -0,0 +1,19 @@
+function move(spaces, direction) {
+  var steps = 0
+  while (steps < spaces) {
+    direction
+    steps = steps + 1
+  }
+}
+function getSet {
+  right
+  var color = getColor
+  down
+  setColor(color)
+  right
+  right
+}
+move(4, down)
+getSet
+up
+getSet
