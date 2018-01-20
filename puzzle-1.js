3b965523077bb8b1072b4e1e80cbd4bb537ef997
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-01-20 15:44:15 -0800

    Update puzzle-1.js

--- puzzle-1.js
+++ puzzle-1.js
@@ -1,9 +1,10 @@
-function move(spaces, direction):
+function move(spaces, direction) {
   var steps = 0
   while (steps < spaces) {
     direction
     steps = steps + 1
   }
+}
 move(7, down)
 move(5, right)
 move(2, up)
