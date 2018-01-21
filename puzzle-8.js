37e17116063db0af1a41ffa95a31be71476c9ef9
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-01-20 16:18:24 -0800

    Create puzzle-8.js

---
+++ puzzle-8.js
@@ -0,0 +1,18 @@
+function move(direction) {
+  var steps = 0
+  while (steps < 7) {
+    direction
+    steps = steps + 1
+  }
+  right
+}
+down
+move(down)
+right
+move(up)
+right
+move(down)
+right
+move(up)
+right
+move(down)
