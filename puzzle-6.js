921fd2067c28a1ca5a8228ff49884660d1507d66
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-01-20 16:35:14 -0800

    Update puzzle-6.js

--- puzzle-6.js
+++ puzzle-6.js
@@ -1,1 +1,28 @@
+right
+if (getColor == "red"){
+  up
+  up
+  up
+  if (getColor == "red") {
+    left
+    up
+  }
+  else {
+    right
+    up
+  }
+}
+else {
+  down
+  down
+  down
+  if (getColor == "red") {
+    left
+    down
+  }
+  else {
+    right
+    down
+}
+
 
