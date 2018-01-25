22bc0d279343c44465b726a7f0cac57a8772b838
ayau3 <35642979+ayau3@users.noreply.github.com>
2018-01-25 10:33:50 -0800

    Create altpuzzle-6.js

---
+++ altpuzzle-6.js
@@ -0,0 +1,24 @@
+right
+var firstColor = getColor
+if(getColor == "blue") {
+  down
+  down
+  down
+else {
+  up
+  up
+  up
+}
+if(getColor == "blue") {
+  right
+}
+else {
+left
+}
+if(firstColor == "blue") {
+  down
+}
+else {
+  up
+}
+  
